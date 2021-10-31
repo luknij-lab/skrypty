695b60d0f54af0df261d3510d0b70c5dac5f09c8
luknij-lab <55351887+luknij-lab@users.noreply.github.com>
2021-10-31 19:51:15 +0100

    Utworzenie pliku hSearch.sh
    Plik do wyszukiwania nagłówków

---
+++ hSearch.sh
@@ -0,0 +1,5 @@
+# Plik powstał 31 paź 2021
+# sposób użycia: bash hSearch.sh [adres_URL_strony_www]
+
+#!/bin/bash
+curl -s -A "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/W.X.Y.Z Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" $1 | xmllint --html --xpath "/html/body//*[self::h1 or self::h2 or self::h3 or self::h4 or self::h5 or self::h6]" - 2>/dev/null | sed -e '0,/<h/ s/<h/\n<h/' | sed 's/^.*\">//g' | sed 's/<h1>//g' | sed 's/<h2>//g' | sed 's/<h3>//g' | sed 's/<h3>//g' | sed 's/<h4>//g' | sed 's/<h5>//g' | sed 's/<h6>//g'
